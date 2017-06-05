b740b168be136405f21b56c6783ddf3bc1a615ef
Anirban Mukhopadhyay <anirban.mukhopadhyay@novu.com>
2017-06-05 17:22:55 -0400

    Finish user signup. End of chapter 7

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid signup info"  do
+    get signup_path
+    assert_select 'form[action="/signup"]', count: 1
+    assert_no_difference 'User.count' do
+      post users_path, params: {user: {name: "",
+                                       email: "user@invalid",
+                                       password: "foo",
+                                       password_confirmation: "bar"}}
+    end
+    assert_template 'users/new'
+    assert_select 'div#error_explanation', count:1
+    assert_select 'div.alert-danger', count:1
+    assert_select 'div.field_with_errors input.form-control', count:4
+  end
+
+  test "valid signup info" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: {name: "Example User",
+                                         email: "user@example.com",
+                                         password: "password",
+                                         password: "password"}}
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div.alert-success', count:1
+    assert_not flash.empty?
+  end
+end
