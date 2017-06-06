2b8c1b2f5571d6394be093dbaeae417c1e616bb1
Anirban Mukhopadhyay <anirban.mukhopadhyay@novu.com>
2017-06-06 16:00:26 -0400

    Implement cookie based login persistence. End of ch 9

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -15,7 +15,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test "login with valid info" do
+  test "login with valid info followed by logout" do
     get login_path
     post login_path, params: {session: {email: @user.email, password: 'password'}}
     assert is_logged_in?
@@ -28,9 +28,33 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    # Simulate a user clicking logout in a loaded second window
+    delete logout_path
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count:0
     assert_select "a[href=?]", user_path(@user), count:0
+  end
+
+  test "login with remembering followed by logout" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+    assert_equal cookies['remember_token'], assigns(:user).remember_token
+
+    # Logout
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path, count:0
+    assert_select "a[href=?]", user_path(@user), count:0
+  end
+
+  test 'login without remembering' do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies['remember_token']
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies['remember_token']
   end
 end
