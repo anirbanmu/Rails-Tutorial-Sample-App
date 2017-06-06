1850b1ac4d2f83e3ad2c5e6e137aa0907c42fc62
Anirban Mukhopadhyay <anirban.mukhopadhyay@novu.com>
2017-06-06 12:21:05 -0400

    Implement basic login; end of ch 8

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -22,11 +22,12 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       post signup_path, params: { user: {name: "Example User",
                                          email: "user@example.com",
                                          password: "password",
-                                         password: "password"}}
+                                         password_confirmation: "password"}}
     end
     follow_redirect!
     assert_template 'users/show'
     assert_select 'div.alert-success', count:1
     assert_not flash.empty?
+    assert is_logged_in?
   end
 end
